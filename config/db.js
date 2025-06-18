61e5f39d651128d652ad99b79cebfde73e1829f6
shaheer-alam-bit <s.alam.25144@khi.iba.edu.pk>
2025-06-19 00:03:22 +0500

    changed conn string

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `${process.env.MONGO_URI}/quickcart`,
+      `${process.env.MONGODB_URI}/quickcart`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
